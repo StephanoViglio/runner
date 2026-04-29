9858d03e6d7d2c1cb84808f21a9193da18a6cc6e
Stephano <stephanoviglio@discente.ufg.br>
2026-04-28 21:00:39 -0300

    chore: organização dos diretórios e arquivos GO

--- assinatura/main.go
+++ assinatura/main.go
@@ -4,7 +4,7 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/StephanoViglio/runner/cmd"
+import "github.com/StephanoViglio/runner/assinatura/cmd"
 
 func main() {
 	cmd.Execute()
