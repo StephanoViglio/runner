2bf9d8a832349894f32b5626c6ea4fe023ae092e
Omar Al Jawabri <164200540+omaraljawabri@users.noreply.github.com>
2026-05-12 17:32:34 -0300

    chore(imports): retirar imports desnecessários da aplicação

--- assinador/src/main/java/com/runner/assinador/dto/request/VerifyRequestDTO.java
+++ assinador/src/main/java/com/runner/assinador/dto/request/VerifyRequestDTO.java
@@ -1,11 +1,7 @@
 package com.runner.assinador.dto.request;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
